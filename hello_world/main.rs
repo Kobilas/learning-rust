d7d02152432687f37c919c5278d19ea2c245659a
Matthew Kobilas <mattkobi789@gmail.com>
2020-04-11 14:51:29 -0400

    Completed 1.2, started 1.3

---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // calls a Rust macro (hence the !)
+    // calling function would be without !
+    println!("Hello, world!");
+}
+// rustc compiles program to .exe (executable file) and .pdb (debug file) on Windows
