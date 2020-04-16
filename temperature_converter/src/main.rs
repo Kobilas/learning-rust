a107d1522dc374176bc6eeebfe9a84254dbca9a2
Matthew Kobilas <mattkobi789@gmail.com>
2020-04-15 22:04:18 -0400

    Completed temperature converter program

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -7,26 +7,46 @@ fn main() {
 
     loop {
         println!("Input F or C, to enter Fahrenheit or Celsius temperature, respectively.");
-        let mut option = String::new();
+        option = String::new();
         io::stdin().read_line(&mut option)
             .expect("Failed to read line.");
-        let option = option.trim();
+        option = option.trim().to_string();
         println!("You entered: {}", option);
 
-        if option == "F" {
-            println!("Fahrenheit temperature will be converted to Celsius.");
-            break;
-        } else if option == "C" {
-            println!("Celsius temperature will be converted to Fahrenheit.");
+        if option == "F" || option == "C" {
             break;
         } else {
             continue;
         }
     }
+
+    loop {
+        println!("Enter temperature to convert from.");
+        let mut temperature = String::new();
+        io::stdin().read_line(&mut temperature)
+            .expect("Failed to read line.");
+        let temperature: f64 = match temperature.trim().parse() {
+            Ok(flt) => flt,
+            Err(_) => continue,
+        };
+        println!("You entered: {}", temperature);
+
+        if option == "F" {
+            println!("Fahrenheit temperature will be converted to Celsius.");
+            println!("{}°F in Celsius is {}°C.", temperature, convert_to_celsius(temperature));
+            break;
+        } else if option == "C" {
+            println!("Celsius temperature will be converted to Fahrenheit.");
+            println!("{}°C in Fahrenheit is {}°F.", temperature, convert_to_fahrenheit(temperature));
+            break;
+        }
+    }
 }
 
-/*
-fn convert_to_fahrenheit(temperature: i32) -> i32 {
-
+fn convert_to_celsius(temperature: f64) -> f64 {
+    (temperature - 32.0) / 1.8
 }
-*/
+
+fn convert_to_fahrenheit(temperature: f64) -> f64 {
+    (temperature * 1.8) + 32.0
+}
