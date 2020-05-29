b4b9d60b95247309745ce099ce52d9d896584aec
Matthew Kobilas <mattkobi789@gmail.com>
2020-05-29 15:21:33 -0400

    miniproject 3 in chapter 8 works

--- employee_management_system/src/main.rs
+++ employee_management_system/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, collections::HashMap};
+use std::{io, collections::{HashMap, hash_map::Entry}};
 
 fn main() {
     let mut ans1 = String::new();
@@ -8,25 +8,54 @@ fn main() {
     loop {
         print_main_menu();
         get_answer(&mut ans1);
-        match &ans1[..] {
+        /*
+         *io::stdin().read_line(&mut ans1)
+         *    .expect("Failed to read line");
+         *ans1 = ans1.trim().to_string();
+         */
+         match &ans1[..] {
             "q" => {
                 println!("Thank you for using the Employee Management System");
                 return;
             },
             "a" => {
+                ans1.clear();
                 println!("Enter department to add employee to:");
                 get_answer(&mut ans1);
-                ems.entry(ans1).or_insert(Vec::new());
+                /*
+                 *io::stdin().read_line(&mut ans1)
+                 *    .expect("Failed to read line");
+                 *ans1 = ans1.trim().to_string();
+                 */
+                //ems.entry(ans1).or_insert(Vec::new());
                 println!("Enter user to add to department {}", ans1);
                 get_answer(&mut ans2);
-                match ems.get(&ans1) {
-                    Some(dept) => dept.push(ans2),
-                    None => {
-                        println!("Error when getting dept {} from ems", ans1);
-                        continue;
-                    }
+                println!("Adding user {} to department {}", ans2, ans1);
+                /*
+                 *io::stdin().read_line(&mut ans2)
+                 *    .expect("Failed to read line");
+                 *ans2 = ans2.trim().to_string();
+                 */
+                /* Previous attempt
+                 *match ems.get(&ans1) {
+                 *    Some(mut dept) => dept.push(&ans2),
+                 *    None => {
+                 *        println!("Error when getting dept {} from ems", ans1);
+                 *        continue;
+                 *    }
+                 *}
+                 */
+                match ems.entry(ans1) {
+                    Entry::Vacant(e) => {
+                        e.insert(vec![ans2]);
+                        //ans1 = e.key().to_string();
+                    },
+                    Entry::Occupied(mut e) => {
+                        e.get_mut().push(ans2);
+                        //ans1 = e.key().to_string();
+                    },
                 }
-                println!("Added user {} to department {}", ans2, ans1);
+                //println!("Added user {} to department {}", ans2, ans1);
             },
             "v" => {
                 println!("Current state of ems:");
@@ -34,6 +63,8 @@ fn main() {
             }
             _ => (),
         }
+        ans1 = String::new();
+        ans2 = String::new();
     }
 }
 
@@ -45,7 +76,7 @@ fn print_main_menu() {
     println!("q - Quit");
 }
 
-fn get_answer(s: &mut String) {
+fn get_answer(mut s: &mut String) {
     io::stdin().read_line(&mut s)
         .expect("Failed to read line");
     *s = s.trim().to_string();
