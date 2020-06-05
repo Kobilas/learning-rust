6f72f5a9fca11ac6083a85dec9f88a237f3a8288
Matthew Kobilas <mattkobi789@gmail.com>
2020-06-05 16:06:57 -0400

    Completed alternative solutions in largest function

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -58,12 +58,21 @@ fn main() {
     println!("The largest number using largest_i32 is {}", result);
     let result = largest(&number_list);
     println!("The largest number using largest is {}", result);
+    let result = largest_clone(&number_list);
+    println!("The largest number using largest_clone is {}", result);
+    let result = largest_reference(&number_list);
+    println!("The largest number using largest_reference is {}", *result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+    let char_list = vec!['m', 'm', 'y', 'q'];
     let result = largest_char(&char_list);
     println!("The largest char using largest_char is {}", result);
-    let result = largest(&number_list);
+    let result = largest(&char_list);
     println!("The largest char using largest is {}", result);
+    let result = largest_clone(&char_list);
+    println!("The largest char using largest_clone is {}", result);
+    let result = largest_reference(&char_list);
+    println!("The largest char using largest_reference is {}", *result);
+
 }
 
 // function can operate on an abstract list instead of specific types
@@ -99,7 +108,7 @@ fn some_function<T, U>(t: T, u: U) -> i32
 */
 
 // Commented out until traits are properly understood
-fn  largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
@@ -109,4 +118,28 @@ fn  largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     largest
 }
 
-// TODO: implement other two solutions given in the book
+// Using Clone trait instead of Copy trait
+fn largest_clone<T: PartialOrd + Clone>(list: &[T]) -> T {
+    let my_list = list.clone();
+    let mut largest = list[0].clone();
+    for item in my_list.iter() {
+        if item > &largest {
+            largest = item.clone();
+        }
+    }
+    largest
+}
+
+// implement without using either Copy nor Clone
+// I don't know how i did thids
+fn largest_reference<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    let mut idx = 0;
+    for (i, item) in list.iter().enumerate() {
+        if item > largest {
+            largest = &item;
+            idx = i;
+        }
+    }
+    &list[idx]
+}
